6585407bc0cb4dd6220d643e88f3860028fe3ec6
v-rex401 <vashtipillay104@gmail.com>
2026-08-29 16:56:21 +0200

    Typed out functions - no implementation yet

--- EventControl.cpp
+++ EventControl.cpp
@@ -5,3 +5,26 @@
  * @brief EventControl is the concrete subject
  * @author Vashti
  */
+
+EventNotice EventControl::getNotice()
+{
+}
+/**
+ * @brief Default Constructor
+ */
+EventControl::EventControl()
+{
+}
+/**
+ * @brief Parameterised Constructor
+ */
+EventControl::EventControl(std::string name)
+{
+}
+
+/**
+ * @brief add function to attach components
+ */
+void EventControl::add(EventComponent *cmp)
+{
+}

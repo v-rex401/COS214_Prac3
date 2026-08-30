c7c0bf77e9c7a2769e1dca904cf5824c911289b5
v-rex401 <vashtipillay104@gmail.com>
2026-08-31 00:25:50 +0200

    EventGroup complete implementation with comments

--- EventGroup.cpp
+++ EventGroup.cpp
@@ -8,7 +8,74 @@
  *
  */
 
+/**
+ * @brief Parameterized Constructor
+ */
 EventGroup::EventGroup(std::string name)
 {
     this->name = name;
+}
+
+/**
+ * @brief Pull update from the subject
+ * This behaviour applies to ConcreteObserver
+ */
+void EventGroup::update()
+{
+    currentNotice = parent->getNotice();
+}
+
+/**
+ * @brief This behaviour applied to ConcreteSubject
+ * @return currentNotice
+ */
+EventNotice EventGroup::getNotice()
+{
+    return currentNotice;
+}
+
+/**
+ * @brief This behaviour applied to ConcreteSubject
+ */
+void EventGroup::setParent(EventSubject *par)
+{
+    parent = par;
+}
+
+/**
+ * @brief This behaviour applied to Composite Design Pattern
+ */
+void EventGroup::add(EventComponent *cmp)
+{
+    componentList.push_back(cmp);
+}
+
+/**
+ * @brief This behaviour applied to Composite Design Pattern
+ */
+void EventGroup::remove(EventComponent *cmp)
+{
+    for (int i = 0; i < componentList.size(); i++)
+    {
+        if (componentList[i] == cmp)
+        {
+            componentList.erase(componentList.begin() + i); /** Shift all the elements to fill the gap  */
+        }
+    }
+}
+
+/**
+ * @brief EventGroup is responsible for its parent pointer
+ * and also the componentList it keeps
+ */
+EventGroup::~EventGroup()
+{
+    delete parent;
+    for (int i = 0; i < componentList.size(); i++)
+    {
+        if (componentList[i] != nullptr)
+        {
+            delete componentList[i];
+        }
+    }
 }

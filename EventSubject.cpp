dd4582473a7fb2de659a21c1d19e2126de241063
Robert-altF4 <robertsibanda17@gmail.com>
2026-08-30 16:07:52 +0200

    Complete EventSubject logic

---
+++ EventSubject.cpp
@@ -0,0 +1,39 @@
+#include "EventSubject.h"
+
+void EventSubject::attach(EventObserver* x){
+    bool duplicate = false;
+
+    if(x != nullptr){
+        for(EventObserver* obs : observerList){
+            if(obs == x){
+            duplicate = true;
+            }
+        }
+
+    }
+    
+
+    if(!duplicate){
+        observerList.push_back(x);
+    }
+}
+
+void EventSubject::detach(EventObserver* y){
+    
+    if(y != nullptr){
+        for(int i = 0; i< this->observerList.size(); i++){
+            if(this->observerList[i] == y){
+                observerList.erase(this->observerList.begin() +i);
+                break;
+            }
+        }
+
+    }
+
+}
+
+void EventSubject::notify(){
+    for(EventObserver* obs : observerList){
+        obs->update();
+    }
+}

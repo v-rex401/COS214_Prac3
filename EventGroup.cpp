baee946d7c0c198c0cbd6dc36f38ce2ce1ff220a
v-rex401 <vashtipillay104@gmail.com>
2026-08-31 00:14:33 +0200

    Constructor for EventGroup

--- EventGroup.cpp
+++ EventGroup.cpp
@@ -1,1 +1,14 @@
 #include "EventGroup.h"
+
+/**
+ * @file EventGroup.cpp
+ * @brief Concrete Subject and Concrete Observer Implementation
+ * @class EventGroup
+ * @author Vashti
+ *
+ */
+
+EventGroup::EventGroup(std::string name)
+{
+    this->name = name;
+}

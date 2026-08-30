4326e81ee48b60cfe12521d744d42481c4c67fe6
v-rex401 <vashtipillay104@gmail.com>
2026-08-30 19:31:57 +0200

    Implementation for EventControl

--- EventControl.cpp
+++ EventControl.cpp
@@ -8,18 +8,27 @@
 
 EventNotice EventControl::getNotice()
 {
+    return currentNotice;
 }
 /**
  * @brief Default Constructor
  */
 EventControl::EventControl()
 {
+    for (int i = 0; i < componentList.size(); i++)
+    {
+        if (componentList[i] != nullptr)
+        {
+            delete componentList[i];
+        }
+    }
 }
 /**
- * @brief Parameterised Constructor
+ * @brief parameterized Constructor
  */
 EventControl::EventControl(std::string name)
 {
+    this->name = name;
 }
 
 /**
@@ -27,4 +36,16 @@ EventControl::EventControl(std::string name)
  */
 void EventControl::add(EventComponent *cmp)
 {
+    componentList.push_back(cmp);
+}
+
+void EventControl::remove(EventComponent *cmp)
+{
+    for (int i = 0; i < componentList.size(); i++)
+    {
+        if (componentList[i] == cmp)
+        {
+            componentList.erase(componentList.begin() + i);
+        }
+    }
 }

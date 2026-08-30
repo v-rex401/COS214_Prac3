7f2bb8cd7d5b95a2850aad1220d82040bcceb50d
v-rex401 <vashtipillay104@gmail.com>
2026-08-31 01:46:50 +0200

    Added logic - TODO: Implementation in EventNotice

--- MainHall.cpp
+++ MainHall.cpp
@@ -5,6 +5,7 @@
  */
 
 #include "EventGroup.h"
+#include "EventNotice.h"
 #include <iostream>
 
 /**
@@ -21,5 +22,31 @@ public:
  */
 void MainHall::print()
 {
-    std::cout << "Welcome to the Main Hall" << std::endl;
+    EventNotice noti = EventGroup::getNotice();
+    enum NoticeType type = noti.getType(); /** @todo Implement getType in EventNotice Class */
+
+    switch (type)
+    {
+    case 0:
+        std::cout << "Main Hall Is open now" << std::endl;
+        break;
+    case 1:
+        std::cout << "Main Hall is closed now" << std::endl;
+        break;
+    case 2:
+        std::cout << "Main Hall has been delayed" << std::endl;
+        break;
+    case 3:
+        std::cout << "Main Hall is too full - proceed to Gaming Hall" << std::endl;
+        break;
+    case 4:
+        std::cout << "Please Evacuate Main Hall" << std::endl;
+        break;
+    case 5:
+        std::cout << "Fire - please move outside" << std::endl;
+        break;
+    case 6:
+        std::cout << "Please stay inside the Main Hall" << std::endl;
+        break;
+    }
 }

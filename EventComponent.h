9dda847af5fc84365052c999ee2a91f33c83f61c
v-rex401 <vashtipillay104@gmail.com>
2026-08-31 10:23:03 +0200

    Added print declaration to the header

--- EventComponent.h
+++ EventComponent.h
@@ -1,16 +1,18 @@
 #ifndef EVENTCOMPONENT_H
 #define EVENTCOMPONENT_H
 
-class EventComponent {
-
+class EventComponent
+{
 
 public:
 	EventComponent();
-	virtual void add(EventComponent* cmp) = 0;
+	virtual void add(EventComponent *cmp) = 0;
 
-	virtual void remove(EventComponent* cmp) = 0;
+	virtual void remove(EventComponent *cmp) = 0;
 
 	virtual ~EventComponent() = default;
+
+	virtual void print() = 0;
 };
 
 #endif
